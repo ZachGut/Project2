bf6c747dec9dd90e918c7bb7c650efd9d27e76dd
ZachGut <zachgut@icloud.com>
2025-04-29 11:20:02 -0500

    Updated main.py
    Added docstrings and type hinting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gui import *
+from gui import Gui, Logic, Tk
 
 """
 (gui.py) Asked AI for assistance on troubleshooting the images not loading which told me how to use the Pillow library to
@@ -7,10 +7,14 @@ load the images.
 which was freezing my entire gui.
 """
 
-def main():
+def main() -> None:
+    """
+    Method to start the application.
+    :return: None
+    """
     window = Tk()
     window.title("CSCI1620-Project2")
-    window.geometry("1100x950")
+    window.geometry("1000x720") #1100x950 for larger screens
     window.resizable(False, False)
     gui = Gui(window, None)
     logic = Logic(gui)
